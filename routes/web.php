68847e0796e2402107355ec16672d4fa6d89de21
Reinaldo Júnior <33222779+ReinaldoJunior96@users.noreply.github.com>
2025-07-08 20:19:37 -0300

    ajustando faceapi

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,8 @@
 use Illuminate\Support\Facades\Route;
 use FaceAuth\Http\Controllers\FaceAuthController;
 
-Route::get('/faceauth/faces', [FaceAuthController::class, 'faces']);
-Route::get('/faceauth/user-image/{id}/{filename}', [FaceAuthController::class, 'userImage']);
-Route::post('/faceauth/login-by-id', [FaceAuthController::class, 'loginById'])->name('faceauth.loginById');
+Route::middleware(['web'])->group(function () {
+   Route::get('/faceauth/faces', [FaceAuthController::class, 'faces']);
+   Route::get('/faceauth/user-image/{id}/{filename}', [FaceAuthController::class, 'userImage']);
+   Route::post('/faceauth/login-by-id', [FaceAuthController::class, 'loginById'])->name('faceauth.loginById');
+});
